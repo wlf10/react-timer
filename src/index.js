de012f6b0e2727a7528f5c328a3f3bccd8264a1a
Valeriy Malyshev <vmalyshev@bitwolff.com>
2018-08-19 21:30:20 +0400

    Update state was corrected

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ class Counter extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            interval: 1,
             time: 0,
             timer: null
         };
@@ -12,26 +13,51 @@ class Counter extends React.Component {
         this.start = this.start.bind(this);
         this.stop = this.stop.bind(this);
         this.reset = this.reset.bind(this);
+
+        this.upIntr = this.upIntr.bind(this);
+        this.downIntr = this.downIntr.bind(this);
     }
 
     start() {
         if (!this.state.timer) {
             this.state.timer = setInterval(() => {
                 this.setState(previousState => {
-                    return { time: previousState.time + 1 };
+                    return { time: previousState.time + this.state.interval };
                 });
-            }, 1000)
+            }, 1000 * this.state.interval)
         }
     }
 
     reset() {
-        // TODO: Тут хотел также остановить счетчик, но данные не обновляются
-        this.state.time = 0;
+        this.setState({
+            time: 0
+        });
     }
 
     stop() {
         clearInterval(this.state.timer);
         this.state.timer = null;
+
+        this.reset();
+    }
+
+    upIntr() {
+        clearInterval(this.state.timer);
+        this.state.timer = null;
+
+        this.state.interval += 1;
+        this.start();
+    }
+
+    downIntr() {
+        if (this.state.interval - 1 > 1) {
+            this.state.interval -= 1;
+
+            clearInterval(this.state.timer);
+            this.state.timer = null;
+
+            this.start();
+        }
     }
 
     secondToTime(sec) {
@@ -53,6 +79,9 @@ class Counter extends React.Component {
                 <div className="button" onClick={this.start}>Start</div>
                 <div className="button" onClick={this.stop}>Stop</div>
                 <div className="button" onClick={this.reset}>Reset</div>
+
+                <div className="button" onClick={this.upIntr}>[+]</div>
+                <div className="button" onClick={this.downIntr}>[-]</div>    
             </div>;
     }
 }
